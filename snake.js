0df3bbb0c52783c9f0b36d0c390ca3feeb7eeaab
Edwin Rivera <edwin.rivera@outlook.com>
2016-11-30 12:47:14 -0500

    created Snake constructor function, added snake control, created food to appear in random location, allow snake to eat the food and create new food when eaten

--- snake.js
+++ snake.js
@@ -4,9 +4,26 @@ function Snake() {
     this.xspeed = 1;
     this.yspeed = 0;
 
+    this.dir = function(x, y){
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.eat = function(pos) {
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     this.update = function() {
-        this.x = this.x + this.xspeed;
-        this.y = this.y + this.yspeed;
+        this.x = this.x + this.xspeed*scl;
+        this.y = this.y + this.yspeed*scl;
+
+        this.x = constrain(this.x, 0, width-scl);
+        this.y = constrain(this.y, 0, height-scl);
     }
 
     this.show = function() {
